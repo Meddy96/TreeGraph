99e534b3ab532b6364a3a32d5d179f7f3efc73d7
Bartosz Swiety <lajza6@gmail.com>
2015-05-24 15:53:59 +0200

    First Commit

---
+++ Assets/GraphSystemScripts/Scrtips/Cell.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+public class Cell : MonoBehaviour 
+{
+	public int id;
+	public RectTransform rTrasfrom;
+	public Text caption;
+	public List<RectTransform> linesTransform;
+	public List<Cell> linkingCells;
+	public List<Cell> connectedCells;
+	public bool visible;
+	public EventTrigger trigger;
+	public GraphMaster master;
+
+	public void RefreshLines()
+	{
+		for (int i = 0; i < linkingCells.Count; i++) 
+		{
+			Vector3 differenceVector = linkingCells[i].rTrasfrom.position - rTrasfrom.position;
+			
+			linesTransform[i].sizeDelta = new Vector2( differenceVector.magnitude, 1);
+			linesTransform[i].pivot = new Vector2(0, 0.5f);
+			linesTransform[i].position = rTrasfrom.position + new Vector3(0,0,-20);
+			float angle = Mathf.Atan2(differenceVector.y, differenceVector.x) * Mathf.Rad2Deg;
+			linesTransform[i].rotation = Quaternion.Euler(0,0, angle);
+
+		}
+
+		foreach (Cell item in connectedCells) 
+		{
+			item.RefreshLines();	
+		}
+		
+	}
+
+	public void Show()
+	{
+		if (visible == false) {
+			visible = true;
+			rTrasfrom.localScale = new Vector3 (0.3F, 0.3F, 0.3F);
+			gameObject.SetActive (true);
+			foreach (Cell item in connectedCells) {
+				item.Show ();	
+			}
+
+			foreach (RectTransform item in linesTransform) {
+				item.gameObject.SetActive (true);
+			}
+		}
+	}
+
+	public void Hide()
+	{
+		foreach (RectTransform item in linesTransform) 
+		{
+			item.gameObject.SetActive(false);
+		}
+		foreach (Cell item in connectedCells) 
+		{
+			item.Hide();	
+		}
+
+		gameObject.SetActive (false);
+		visible = false;
+	}
+
+	public void Minimize()
+	{
+		visible = false;
+		foreach (Cell item in connectedCells) 
+		{
+			item.Hide();	
+		}
+		rTrasfrom.localScale = new Vector3 (0.05F, 0.05F, 0.05F);
+	}
+
+	public void OnPointer()
+	{
+		master.NewSelectedCell (this);
+	}
+
+}
---
+++ Assets/GraphSystemScripts/Scrtips/GraphMaster.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+using UnityEngine.EventSystems;
+
+public class GraphMaster : MonoBehaviour 
+{
+
+	public List<Cell> cells;
+	public Object cellPrefab;
+	public Object linePrefab;
+
+	public Transform cellsParent;
+	public Transform linesParent;
+
+	private Vector3 lastMousePosition;
+	private Vector3 deltaMousePosition;
+
+	public Cell selectedCell;
+	public Cell dynamicCell;
+	public Cell creatingLineCell;
+
+
+
+	void Update () 
+	{
+		//Desktop controlling
+	
+		deltaMousePosition = Input.mousePosition - lastMousePosition;
+		lastMousePosition = Input.mousePosition;
+
+		if (Input.GetMouseButton (0))
+		{
+			GameObject selected = EventSystem.current.currentSelectedGameObject;
+			if(selected!= null)
+			{
+				if(selected.tag == "CellUI")
+					selected = selected.transform.parent.gameObject;
+
+				if(selected.tag == "Cell")
+				{
+					Cell movedCell = selected.GetComponent<Cell>();		movedCell.RefreshLines();
+					movedCell.transform.position += deltaMousePosition;
+					movedCell.RefreshLines();
+				}
+			}
+
+		}
+
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			creatingLineCell = selectedCell;
+			CreateLine(creatingLineCell);
+		}
+
+		if (Input.GetMouseButton (1)) 
+		{
+			creatingLineCell.linkingCells[creatingLineCell.linkingCells.Count-1].rTrasfrom.position = Input.mousePosition;
+			creatingLineCell.RefreshLines();
+		}
+
+		if (Input.GetMouseButtonUp (1)) 
+		{
+			if(selectedCell != creatingLineCell)
+			{
+				creatingLineCell.linkingCells[creatingLineCell.linkingCells.Count-1] = selectedCell;
+				selectedCell.connectedCells.Add(creatingLineCell);
+			}
+			else
+			{
+				Destroy(creatingLineCell.linesTransform[creatingLineCell.linkingCells.Count-1].gameObject);
+				creatingLineCell.linesTransform.RemoveAt(creatingLineCell.linkingCells.Count-1);
+				creatingLineCell.linkingCells.RemoveAt(creatingLineCell.linkingCells.Count-1);
+				creatingLineCell.RefreshLines();
+			}
+		}
+
+		//to fix
+		//transform.parent.localScale += new Vector3 (Input.GetAxis ("Mouse ScrollWheel"), Input.GetAxis ("Mouse ScrollWheel"), 0);
+
+
+	}
+
+	public void CreateNewCell()
+	{
+		Cell newCell = (Instantiate (cellPrefab, new Vector3 (Screen.width / 2, Screen.height / 2, 0), Quaternion.identity) as GameObject).GetComponent<Cell> ();
+		newCell.transform.parent = cellsParent;
+		newCell.master = this;
+	}
+
+	public void CreateLine(Cell parent)
+	{
+		GameObject newLine = Instantiate (linePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+		newLine.transform.parent = linesParent;
+		parent.linesTransform.Add (newLine.GetComponent<RectTransform>());
+		parent.linkingCells.Add(dynamicCell);
+	}
+
+
+	public void NewSelectedCell(Cell cell)
+	{
+		selectedCell = cell;
+	}
+
+}
